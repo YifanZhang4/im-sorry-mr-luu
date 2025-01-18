9641c948c1b700c2afa80cf5c88630c48af01403
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2025-01-17 22:31:02 -0500

    more fixes plus death'

--- luu/src/components/GameComponent.vue
+++ luu/src/components/GameComponent.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="sans">
+    <div v-if="health === 0">
+      <div role="alert" class="alert alert-error">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6 shrink-0 stroke-current"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <span>Wow! You Lost! Congrats! Please Imagine Me Clapping!</span>
+      </div>
+    </div>
     <div class="flex justify-center">
       <img
         src="../../public/icons/1.png"
@@ -164,7 +182,10 @@ const submitGuess = () => {
   }
 
   if (health.value === 0) {
-    location.reload();
+    const reload = () => {
+      window.location = window.location;
+    };
+    setInterval(reload, 3000);
   }
 };
 
